feec1631f63f3e8a3043691b6a0596a6946aee6e
Vaibhav Bhute <vaibhavbhute2021@gmail.com>
2024-07-10 23:18:00 +0530

    Auto-generated commit

--- chunk-WYX5CESN.js
+++
@@ -1,1 +0,0 @@
-import{$ as n,$a as i,Ra as o,Sa as r,bb as s}from"./chunk-UKGNGN7I.js";var f=(()=>{let e=class e{};e.\u0275fac=function(a){return new(a||e)},e.\u0275cmp=n({type:e,selectors:[["app-dashboard"]],standalone:!0,features:[s],decls:2,vars:0,template:function(a,p){a&1&&(o(0,"p"),i(1,"dashboard works!"),r())}});let t=e;return t})();export{f as default};
